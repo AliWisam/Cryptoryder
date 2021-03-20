b4b0ecfcba607438d7629e87324a9ad24da6aad6
AliWisam <aliwisaam73@gmail.com>
2021-03-21 01:31:25 +0500

    removing erros and adding files with migrations

---
+++ contracts/zeppelin/lifecycle/Killable.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.5.16;
+
+
+//import "./../ownership/Ownable.sol";
+import "contracts/zeppelin/ownership/Ownable.sol";
+
+
+/*
+ * Killable
+ * Base contract that can be killed by owner. All funds in contract will be sent to the owner.
+ */
+// contract Killable is Ownable {
+//   function kill() onlyOwner {
+//     selfdestruct(owner);
+//   }
+// }
+
+contract Killable is Ownable {
+  function kill() public onlyOwner{
+     address owner;
+    address payable addr3 = address(uint160(owner));
+    
+    selfdestruct(addr3);
+  }
+}
